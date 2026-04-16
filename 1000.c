ae295f5198ae1e676566f1827ffdfd633b4c23b8
rinconaline <aline.rincon@unifesp.br>
2026-04-16 10:46:30 -0300

    Create 1000.c

---
+++ 1000.c
@@ -0,0 +1,17 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Plataforma  : https://judge.beecrowd.com/pt/search
+Exercício   : 1000
+Data        : 07/04/2026
+Objetivo    : Imprimir "Hello World!".
+Aprendizado : Como imprimir uma mensagem.
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+ 
+int main() {
+ 
+    printf("Hello World!\n");
+ 
+    return 0;
+}
