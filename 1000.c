fcc26486a9914d3032d90f47c0129d1f53f8180c
rinconaline <aline.rincon@unifesp.br>
2026-04-17 09:49:56 -0300

    Update 1000.c

--- 1000.c
+++ 1000.c
@@ -1,8 +1,8 @@
 /* --------------------------------------------------------------------------
 Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Aline Mariano Rincon
 Linguagem   : C
-Plataforma  : https://judge.beecrowd.com/pt/search
-Exercício   : 1000
+Programa    : https://judge.beecrowd.com/en/problems/view/1000
 Data        : 07/04/2026
 Objetivo    : Imprimir "Hello World!".
 Aprendizado : Como imprimir uma mensagem.
