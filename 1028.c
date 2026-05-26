c4964b282b3bb0c340b3d17e8fcfdf3cda635d48
Aline Rincon <aline.rincon@unifesp.br>
2026-05-26 16:28:06 -0300

    Add program to compute GCD of pairs of numbers

---
+++ 1028.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, f1, f2, resto;
+    
+    scanf("%d", &n);
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &f1, &f2);
+       
+        while (f2 != 0) {
+            resto = f1 % f2;
+            f1 = f2;
+            f2 = resto;
+        }
+        
+       
+        printf("%d\n", f1);
+    }
+    
+    return 0;
+}
