78aedf4d8cdff64cbcf59ccb80f400882d28bd5e
Aline Rincon <aline.rincon@unifesp.br>
2026-06-16 16:55:52 -0300

    Add header comments to 1435.c
    Added header comments with program details and objectives.

--- 1435.c
+++ 1435.c
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Aline Mariano Rincon
+Linguagem   : C
+Programa    : https://judge.beecrowd.com/en/problems/view/1435
+Data        : 16/06/2026
+Objetivo    : Construir uma matriz quadrada de ordem N onde os elementos formam camadas concêntricas com valores crescentes a partir das bordas externas.
+Aprendizado : Manipulação de matrizes bidimensionais e uso da lógica de distância mínima até as quatro bordas para determinação dos valores dos elementos.
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 int main() {
