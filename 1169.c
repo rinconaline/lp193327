fbc2793e31f99b144267202e5f80f814036540c6
Aline Rincon <aline.rincon@unifesp.br>
2026-06-16 16:53:52 -0300

    Update 1169.c

--- 1169.c
+++ 1169.c
@@ -1,4 +1,12 @@
-
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Aline Mariano Rincon
+Linguagem   : C
+Programa    : https://judge.beecrowd.com/en/problems/view/1169
+Data        : 16/06/2026
+Objetivo    : Calcular a quantidade de quilos de trigo acumulados em um tabuleiro de xadrez usando uma função recursiva de potência de base 2.
+Aprendizado : Aplicação de recursividade, uso de tipos de dados de 64 bits sem sinal (unsigned long long) e conversão de unidades (grãos para gramas e quilos).
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 unsigned long long graos (int quadrados){
