75db94b21aec3725a123680fb433dc4af4c5ffe2
Aline Rincon <aline.rincon@unifesp.br>
2026-06-16 16:56:33 -0300

    Add header comments for Collatz conjecture program
    Add header comments with problem details and objectives

--- 1441.c
+++ 1441.c
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Aline Mariano Rincon
+Linguagem   : C
+Programa    : https://judge.beecrowd.com/en/problems/view/1441
+Data        : 16/06/2026
+Objetivo    : Simular a sequência da Conjectura de Collatz (problema 3n + 1) para um valor inicial 'h' e identificar o maior valor atingido durante o processo.
+Aprendizado : Implementação de estruturas de repetição condicionais complexas, aplicação de regras aritméticas baseadas na paridade de um número e controle de máximos globais.
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 int main() {
